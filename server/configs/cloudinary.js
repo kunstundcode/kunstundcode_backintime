34dd729c8e86136498cbd85aa27370fb4855baa3
André Sebastian <a.sebastian@posteo.de>
2020-07-12 09:38:52 +0200

    update cloudinary config

--- server/configs/cloudinary.js
+++ server/configs/cloudinary.js
@@ -1,5 +1,5 @@
 const cloudinary = require('cloudinary');
-const cloudinaryStorage = require('multer-storage-cloudinary');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
 
 cloudinary.config({
@@ -8,7 +8,7 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_SECRET
 });
 
-var storage = cloudinaryStorage({
+var storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   folder: 'KunstundCode', // The name of the folder in cloudinary
   allowedFormats: ['jpg', 'png'],
